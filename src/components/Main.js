60e5989553e5fe3c53dda961c3e4831b375cc1c0
kaechesbord <damirlihtneri6@gmail.com>
2022-11-04 14:12:31 +0100

    Finished

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from "react";
 import Swal from 'sweetalert2'
-
 const Main = () => {
-  const [num1, setNum] = useState(1);
-  const [num2, setNum2] = useState(1);
+  const [num1, setNum] = useState(2);
+  const [num2, setNum2] = useState(2);
   const current = num1 + num2;
   const [player1, setPlayer1] = useState({
     isActive: true,
-    currentScore: current,
+    currentScore: 0,
     totalScore: 0,
   });
   const [player2, setPlayer2] = useState({
     isActive: false,
-    currentScore: current,
+    currentScore: 0,
     totalScore: 0,
   });
   function switchActive() {
@@ -25,11 +24,13 @@ const Main = () => {
       setPlayer1({...player1, isActive: true})
     }
   }
-  function math() {
+  function diceDecider() {
     const randomNum = Math.floor(Math.random() * 6) + 1;
     const randomNum2 = Math.floor(Math.random() * 6) + 1;
     setNum(randomNum);
     setNum2(randomNum2);
+  }
+  function increaseScore() {
     if (player1.isActive) {
       player1.totalScore += current;
       setPlayer2({...player2, currentScore: 0})
@@ -41,20 +42,59 @@ const Main = () => {
       setPlayer1({...player1, currentScore: 0})
     }
   }
+  function rollDie() {
+    diceDecider()
+    increaseScore()
+  }
   useEffect(() => {
-    math();
-  }, []);
+    diceDecider()
+  },[])
   useEffect(() => {
-    const timer = setTimeout(() =>
-    Swal.fire(
-      'The Rules?',
-      'That thing is still around?',
-      'question'
-    ), 1000);
+Swal.fire({
+  icon: 'info',
+  title: 'RULES',
+  text: 'First to get to 100 wins, get 1 on either die and the player switches.',
+  background: '#fff ',
+  backdrop: `
+  #964000
+  `
+})
   }, []);
-  function refreshPage() {
+  function newGame() {
     window.location.reload(false);
   }
+  function ruleEnforce() {
+    if(num1 === 1 || num2 === 1) {
+      setTimeout(() => {
+        switchActive();
+        diceDecider();
+      },400)
+    }
+  }
+  ruleEnforce()
+  function prize() {
+    Swal.fire({
+      title: 'You Won!',
+      text: "Up for another round?",
+      icon: 'success',
+      backdrop: `
+        #00FF7F`,
+      showConfirmButton: true,
+      confirmButtonColor: '#3085D6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sure!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+       newGame();
+      }
+    })
+  }
+  function winnerDecider() {
+    if((player1.isActive && player1.totalScore >= 100) || (player2.isActive && player2.totalScore >= 100)) {
+      prize();
+    }
+  }
+  winnerDecider()
   return (
     <div>
       <div className="wrapper clearfix">
@@ -72,7 +112,6 @@ const Main = () => {
             </div>
           </div>
         </div>
-
         <div className={player2.isActive ? "player-0-panel active" : "player-1-panel" }>
           <div className="player-name" id="name-1">
             Player 2
@@ -87,19 +126,16 @@ const Main = () => {
             </div>
           </div>
         </div>
-
-        <button className="btn-new" onClick={refreshPage}>
+        <button className="btn-new" onClick={newGame}>
           <i className="ion-ios-plus-outline"></i>New game
         </button>
-        <button className="btn-roll" onClick={math}>
+        <button className="btn-roll" onClick={rollDie}>
           <i className="ion-ios-loop"></i>Roll dice
         </button>
         <button className="btn-hold" onClick={switchActive}>
           <i className="ion-ios-download-outline"></i>Hold
         </button>
-
         <input type="text" id="winningScore" placeholder="Final score" />
-
         <img
           src={require(`../components/images/dice-${num1}.png`)}
           alt="Dice"
@@ -116,5 +152,4 @@ const Main = () => {
     </div>
   );
 };
-
 export default Main;
